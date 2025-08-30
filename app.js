d69647a8d01e1f677162b222a765e8a15b779ceb
Deepanshu Patidar <patidardeepanshu910@gmail.com>
2025-08-30 16:38:47 +0530

    mongoDb set and connection

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import {PORT} from './config/env.js';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import subscriptionRoutes from './routes/subscription.route.js';
+import connectToDatabase from './database/mongodb.js';
+
 const app = express();
 
 app.use(express.json());
@@ -11,8 +13,9 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/subscriptions", subscriptionRoutes);
 
-app.listen(PORT, ()  => {
+app.listen(PORT, async ()  => {
     console.log(`Server is running on port http://localhost:${PORT}`);
+    await connectToDatabase();
 });
 
 export default app;   
