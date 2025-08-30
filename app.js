838a60f6811f192e42bd277740c160317dfb8d77
Deepanshu Patidar <patidardeepanshu910@gmail.com>
2025-08-30 14:37:58 +0530

    env setup

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("server running at 5000");
+import express from 'express';
+import {PORT} from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('welcome to the home page');
+})
+
+app.listen(PORT, ()  => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
+export default app;   
