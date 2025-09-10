c61ead3b87add11bd9832ca89aa2bea630ad1c08
Deepanshu-0ne8 <patidardeepanshu910@gmail.com>
2025-09-10 18:05:27 +0530

    authorization creation and middleware updation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
-    res.send({
-        title: 'GET Users'
-    })
-});
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) => {
-    res.send({
-        title: 'GET User details'
-    })
-});
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => {
     res.send({
